bf02281f76aeaa609a0c5e4f3b861e00d68cdb54
estherattar1 <106767420+estherattar1@users.noreply.github.com>
2022-06-28 12:50:56 -0400

    Update app.py

--- skeleton/app.py
+++ skeleton/app.py
@@ -126,8 +126,8 @@ def register_user():
 		firstname=request.form.get('firstname')
 		lastname=request.form.get('lastname')
 		hometown=request.form.get('hometown')
-        gender=request.form.get('gender')
-        birthdate = request.form.get('birthdate')
+		gender=request.form.get('gender')
+		birthdate = request.form.get('birthdate')
 	except:
 		print("couldn't find all tokens") #this prints to shell, end users will not see this (all print statements go to shell)
 		return flask.redirect(flask.url_for('register'))
