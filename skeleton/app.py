436057f55cb8ef6c884be7b10b6dcdb2deb79a34
estherattar1 <106767420+estherattar1@users.noreply.github.com>
2022-06-27 20:13:04 -0400

    Update app.py

--- skeleton/app.py
+++ skeleton/app.py
@@ -23,7 +23,7 @@ app.secret_key = 'super secret string'  # Change this!
 
 #These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'p301102s'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Ela123456'
 app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
